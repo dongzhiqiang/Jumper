f34ce98d02adacda496b471f0f750c8e555bb1fb
dongzhiqiang <d_zhi_q@163.com>
2017-01-17 18:06:35 +0800

    结构调整

--- Game/Assets/Game/Scripts/Main.cs
+++ Game/Assets/Game/Scripts/Main.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Main : MonoBehaviour {
+/// <summary>
+/// </summary>
+public class Main : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        AppFacade.Instance.StartUp();   //启动游戏
+    }
 }
